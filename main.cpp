566bd6c7a6c8834450255de63cd05fc9a6151b43
RuoguL <127040328+RuoguL@users.noreply.github.com>
2023-03-05 16:33:27 +0000

    Add the first file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Welcome, my friend!" << endl;
+    int x;
+    for(int i=0;i<10;i++){
+        x+=x;
+    }
+	return 0;
+}
